b4af3042c34c2eab960dc723c0bb5caa3d50a70a
Brenden Pelkie <bgpelkie@gmail.com>
2023-03-07 12:11:55 -0800

    change chart size

--- streamlitapp.py
+++ streamlitapp.py
@@ -8,7 +8,7 @@ vendables = pd.read_csv('purchaseable_with_tsne.csv')
 #vendables['image'] = vendables['CID'].apply(lambda x: f'http://localhost:8501/app/static/altair_images/{x}.png')
 
 
-chart = alt.Chart(vendables, width = 800, height = 800).mark_circle(size=100).encode(
+chart = alt.Chart(vendables, width = 500, height = 500).mark_circle(size=100).encode(
     x = 'tsne_1',
     y = 'tsne_2',
     tooltip=['CID', 'image'],
